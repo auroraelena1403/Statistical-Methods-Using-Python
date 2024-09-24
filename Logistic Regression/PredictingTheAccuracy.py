a03788c7d82c1ba8646173c33e384ebc7c9843ec
Aurora <156342985+auroraelena1403@users.noreply.github.com>
2024-09-24 14:29:51 +0300

    Add files via upload

---
+++ Logistic Regression/PredictingTheAccuracy.py
@@ -0,0 +1,86 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# Importing the libraries
+
+# In[2]:
+
+
+import pandas as pd
+import numpy as np
+import statsmodels.api as sm
+import matplotlib.pyplot as plt
+import seaborn as sns
+sns.set()
+
+
+# Loading the data
+
+# In[5]:
+
+
+raw_data=pd.read_csv('C:\\Users\\elena\\Downloads\\2.02.+Binary+predictors.csv')
+raw_data
+
+
+# Mapping the values
+
+# In[10]:
+
+
+data=raw_data.copy()
+data['Admitted']=data['Admitted'].map({'Yes':1,'No':0})
+data['Gender']=data['Gender'].map({'Female':1,'Male':0})
+data
+
+
+# Declaring the dependent and independent variables
+# 
+
+# In[21]:
+
+
+y=data['Admitted']
+x1=data[['SAT','Gender']]
+
+
+# Logistic Regression
+
+# In[23]:
+
+
+x=sm.add_constant(x1)
+reg_log=sm.Logit(y,x)
+result_log=reg_log.fit()
+result_log.summary()
+
+
+# The odds of a female being admitted: np.exp(genderCoeffValue)
+
+# Predicting the accuracy of the model in 2 ways:
+
+# In[29]:
+
+
+result_log.pred_table()
+cm_df=pd.DataFrame(result_log.pred_table())
+cm_df.columns=['Predicted 0','Predicted1']
+cm_df=cm_df.rename(index={0:'Actual 0',1:'Actual 1'})
+cm_df
+
+
+# In[31]:
+
+
+cm=np.array(cm_df)
+accuracy=(cm[0,0]+cm[1,1])/cm.sum()
+accuracy
+
+
+# 94.6 accuracy
+
+# In[ ]:
+
+
+
+
